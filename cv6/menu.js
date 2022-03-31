fc3d019ce765e90ce7d934d0095ee9c6a4027c9e
Tomas Bartu <tomasbartu00@gmail.com>
2022-03-31 17:55:07 +0200

    final :zap:

--- cv6/menu.js
+++ cv6/menu.js
@@ -1,7 +1,10 @@
 var lever = true;
+
 document.getElementById('hamburger').addEventListener('click', hide);
 
 window.addEventListener('resize', resizing);
+
+title();
 
 var navbar = document.getElementById("navbar");
 var menu = document.getElementById("ul");
@@ -14,9 +17,10 @@ window.onscroll = function() {
     }
 }
 
+
 function hide() {
     var list = document.getElementsByClassName('list');
-    for (var i = 1; i < list.length; i++) {
+    for (var i = 0; i < list.length; i++) {
         if (lever) {
             list[i].style.display = 'block';
         } else {
@@ -34,27 +38,81 @@ function hide() {
     lever = !lever;
 }
 
+const links = document.querySelectorAll("#navbar ul a");
+
+for (const link of links) {
+    link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+    e.preventDefault();
+    const href = this.getAttribute("href");
+    const offsetTop = document.querySelector(href).offsetTop;
+    var width = document.getElementById('body').offsetWidth;
+    if (width <= 766) {
+        scroll({
+            top: offsetTop - 350,
+            behavior: "smooth"
+        });
+        hide();
+    } else {
+        scroll({
+            top: offsetTop - 50,
+            behavior: "smooth"
+        });
+    }
+}
+
+function title() {
+    var width = document.getElementById('body').offsetWidth;
+    const about = document.getElementById("li_about");
+    const lectures = document.getElementById("li_lectures");
+    const results = document.getElementById("li_results");
+    const exercises = document.getElementById("li_exercises");
+    const contact = document.getElementById("li_contact");
+    const related = document.getElementById("li_related");
+
+    if (width > 767 && width < 991) {
+        about.title = 'O předmětu';
+        lectures.title = 'Přednášky';
+        results.title = 'Výsledky';
+        exercises.title = 'Cvičení';
+        contact.title = 'Kontakt';
+        related.title = 'Související';
+    }
+    if (width > 991) {
+        about.title = '';
+        results.title = '';
+        exercises.title = '';
+        contact.title = '';
+        related.title = '';
+    }
+}
+
+
 function resizing() {
-    var width = this.window.screen.availWidth;
-    if (width < 766) {
+    var width = document.getElementById('body').offsetWidth;
+    title();
+
+    if (width <= 766) {
         var list = document.getElementsByClassName('list');
-        for (var i = 1; i < list.length; i++) {
+        for (var i = 0; i < list.length; i++) {
             list[i].removeAttribute('style');
         }
     }
     if (width > 767 && width < 991) {
         var list = document.getElementsByClassName('list');
-        for (var i = 1; i < list.length; i++) {
+        for (var i = 0; i < list.length; i++) {
             list[i].removeAttribute('style');
         }
         var spanik = document.getElementsByClassName('spanik');
-        for (var i = 1; i < spanik.length; i++) {
+        for (var i = 0; i < spanik.length; i++) {
             spanik[i].removeAttribute('style');
         }
     }
     if (width > 991) {
         var spanik = document.getElementsByClassName('spanik');
-        for (var i = 1; i < spanik.length; i++) {
+        for (var i = 0; i < spanik.length; i++) {
             spanik[i].removeAttribute('style');
         }
     }
